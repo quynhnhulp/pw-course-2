e084e30e079e1a4cabd99637db9daf89b1d5c75b
PW-Nhu <quynhnhulp55@gmail.com>
2025-07-22 23:01:17 +0700

    add ex1.js file

---
+++ tests/lesson-2/ex1.js
@@ -0,0 +1,9 @@
+const number = 12;
+let name = "my number";
+let isEven = false;
+
+if(number % 2 === 1){
+    isEven = true;
+}
+console.log("kiểm tra hằng số number là số chẵn:", number);
+console.log("kiểm tra biến isEven:", isEven);
